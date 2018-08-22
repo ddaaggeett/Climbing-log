a04d8bcda8e617bba5d31500553fa62d6b833b95
ddaaggeett <david.g.daggett@gmail.com>
2018-08-22 09:50:42 -0400

    auto generate database and tables

--- src/db/index.js
+++ src/db/index.js
@@ -9,49 +9,51 @@ var db_host = config.get('host')
 var socket_port = config.get('socketPort')
 var db_port = config.get('dbPort')
 var db = config.get('db')
-var db_table = config.get('table')
+var tables = config.get('tables')
 
 var ioServer = app.listen(process.env.PORT || socket_port, function() {
     var host = db_host
-    var port = ioServer.address().port;
-    console.log('socket.io listening at http://' + host + ':' + port);
-});
+    var port = ioServer.address().port
+    console.log('socket.io listening at http://' + host + ':' + port)
+})
 var io = require('socket.io')(ioServer, {pingTimeout: 1})
 
 r.connect({
     host: db_host,
     port: db_port,
     db: db
+}, function (err, connection) {
+    r.dbCreate(db).run(connection, function(err, result) {
+        if(err) console.log("[DEBUG] RethinkDB database '%s' already exists (%s:%s)\n%s", db, err.name, err.msg, err.message)
+        else console.log("[INFO ] RethinkDB database '%s' created", db)
+        for(var tbl in tables) {
+            (function (tableName) {
+                r.db(db).tableCreate(tableName).run(connection, function(err, result) {
+                    if(err) console.log("[DEBUG] RethinkDB table '%s' already exists (%s:%s)\n%s", tableName, err.name, err.msg, err.message)
+                    else console.log("[INFO ] RethinkDB table '%s' created", tableName)
+                })
+            })(tbl)
+        }
+    })
 }).then(function(connection) {
 
 	io.sockets.on('connection', function (socket) {
 
         socket.on(actions.UPDATE_USER_INST, function(newUserInst) {
-            console.log('client trigger\n',newUserInst)
             try {
-                r.table(db_table).get(newUserInst.id).update(newUserInst).run(connection);
+                r.table(tables.user).get(newUserInst.id).update(newUserInst).run(connection)
             }
             catch(err) {
-                r.table(db_table).insert(newUserInst).run(connection);
-            }
-        })
-
-        socket.on(actions.UPDATE_WALL, function(newWall) {
-            console.log('trigger wall change')
-            try {
-                r.table(db_table).get(newWall.id).update(newWall).run(connection);
-            }
-            catch(err) {
-                r.table(db_table).insert(newWall).run(connection);
+                r.table(tables.user).insert(newUserInst).run(connection)
             }
         })
 
         /*
         RethinkDB changefeed
         */
-        r.table(db_table).changes({ includeInitial: true, squash: true }).run(connection).then(changefeeds.changefeeds(socket));
-	});
+        r.table(tables.user).changes({ includeInitial: true, squash: true }).run(connection).then(changefeeds.changefeeds(socket))
+	})
 })
 .error(function(error) {
-	console.log('Error connecting to RethinkDB!\n',error);
-});
+	console.log('Error connecting to RethinkDB!\n',error)
+})
