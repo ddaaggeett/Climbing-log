27a47c4889023ed8637fcaca9c13bb389f2ae7b8
ddaaggeett <david.g.daggett@gmail.com>
2018-08-27 22:14:33 -0400

    toggle wall succeeded works

--- src/components/RockWall.js
+++ src/components/RockWall.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Text, View, TextInput, Switch } from 'react-native';
+import * as actions from '../actions'
 import { styles } from '../styles'
+import rnConfig from '../../config/rnConfig' // TODO: use a single source for configs
+import io from 'socket.io-client/dist/socket.io'
+const socket = io.connect('http://' + rnConfig.serverIP + ':' + rnConfig.socketPort)
 
 export default class RockWall extends Component {
     constructor(props) {
@@ -12,27 +16,27 @@ export default class RockWall extends Component {
         }
     }
     handleToggleWallSuccess = (value) => {
-        this.setState({
-            succeeded: value
-        })
-    }
-    componentWillMount() {
-        this.handleWallScan()
+        const newUserInst = {
+            ...this.props.user,
+            walls: [
+                {
+                    ...this.props.user.walls[0],
+                    succeeded: value
+                },
+                ...this.props.user.walls.slice(1)
+            ]
+        }
+        socket.emit(actions.UPDATE_USER_INST, newUserInst)
     }
     render() {
         return (
             <View style={styles.container}>
-                <Text style={[styles.text,styles.screenHeader]}>wall: {this.state.wall}</Text>
+                <Text style={[styles.text,styles.screenHeader]}>wall: {this.props.user.walls[0].id}</Text>
                 <Text style={styles.text}>Climber: {this.props.user.name}</Text>
-                <Text style={styles.text} className='succeeded'>Succeeded?</Text>
-                <Switch className='succeeded'
-                    onValueChange = {this.handleToggleWallSuccess}
-                    value = {this.state.succeeded} />
+                <Text style={styles.text}>Succeeded?</Text>
+                <Switch onValueChange = {this.handleToggleWallSuccess}
+                    value = {this.props.user.walls[0].succeeded} />
             </View>
         )
-    }
-
-    handleWallScan() {
-
     }
 }
