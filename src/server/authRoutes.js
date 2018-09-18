dfb5179b44e3439ab39bb0d01539b17e54507784
ddaaggeett <david.g.daggett@gmail.com>
2018-09-18 14:52:46 -0400

    passport authentication (authRoutes.js) redirect requires new intent-filter in AndroidManifest.xml file

--- src/server/authRoutes.js
+++ src/server/authRoutes.js
@@ -8,6 +8,6 @@ app.get(
     '/auth/github/callback',
     passport.authenticate('github', { failureRedirect: '/auth/github' }),
     (req, res) => {
-        res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user))
+        res.redirect('climbinglog://login?user=' + JSON.stringify(req.user))
     }
 )
